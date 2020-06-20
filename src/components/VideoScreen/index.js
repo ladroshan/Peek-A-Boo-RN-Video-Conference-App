ef1c77c15bac0ee1c647624b400d7c93a10354df
Pavanaditya Mylavarapu <M1050962@mindtree.com>
2020-06-21 01:15:23 +0530

    login, register views added to auth page

--- src/components/VideoScreen/index.js
+++ src/components/VideoScreen/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { SafeAreaView, StatusBar } from 'react-native';
+import {SafeAreaView, StatusBar} from 'react-native';
 import ConnectyCube from 'react-native-connectycube';
 import AwesomeAlert from 'react-native-awesome-alerts';
 import RTCViewGrid from './RTCViewGrid';
-import { CallService, AuthService } from '../../services';
+import {CallService, AuthService} from '../../services';
 import ToolBar from './ToolBar';
 import UsersSelect from './UsersSelect';
 
@@ -13,6 +13,8 @@ export default class VideoScreen extends React.Component {
 
     this._session = null;
     this.opponentsIds = props.navigation.getParam('opponentsIds');
+    this.currentUserLoginId = props.navigation.getParam('currentUserLoginId');
+    this.currentUserFullName = props.navigation.getParam('currentUserFullName');
 
     this.state = {
       localStream: null,
@@ -26,12 +28,12 @@ export default class VideoScreen extends React.Component {
     this._setUpListeners();
   }
 
-  componentWillUnmount () {
+  componentWillUnmount() {
     CallService.stopCall();
     AuthService.logout();
   }
 
-  componentDidUpdate (prevProps, prevState) {
+  componentDidUpdate(prevProps, prevState) {
     const currState = this.state;
 
     if (
@@ -45,12 +47,12 @@ export default class VideoScreen extends React.Component {
 
   showInomingCallModal = session => {
     this._session = session;
-    this.setState({ isIncomingCall: true });
+    this.setState({isIncomingCall: true});
   };
 
   hideInomingCallModal = () => {
     this._session = null;
-    this.setState({ isIncomingCall: false });
+    this.setState({isIncomingCall: false});
   };
 
   selectUser = userId => {
@@ -66,11 +68,11 @@ export default class VideoScreen extends React.Component {
   };
 
   closeSelect = () => {
-    this.setState({ isActiveSelect: false });
+    this.setState({isActiveSelect: false});
   };
 
   setOnCall = () => {
-    this.setState({ isActiveCall: true });
+    this.setState({isActiveCall: true});
   };
 
   initRemoteStreams = opponentsIds => {
@@ -79,31 +81,31 @@ export default class VideoScreen extends React.Component {
       stream: null,
     }));
 
-    this.setState({ remoteStreams: emptyStreams });
+    this.setState({remoteStreams: emptyStreams});
   };
 
   updateRemoteStream = (userId, stream) => {
-    this.setState(({ remoteStreams }) => {
+    this.setState(({remoteStreams}) => {
       const updatedRemoteStreams = remoteStreams.map(item => {
         if (item.userId === userId) {
-          return { userId, stream };
+          return {userId, stream};
         }
 
-        return { userId: item.userId, stream: item.stream };
+        return {userId: item.userId, stream: item.stream};
       });
 
-      return { remoteStreams: updatedRemoteStreams };
+      return {remoteStreams: updatedRemoteStreams};
     });
   };
 
   removeRemoteStream = userId => {
-    this.setState(({ remoteStreams }) => ({
+    this.setState(({remoteStreams}) => ({
       remoteStreams: remoteStreams.filter(item => item.userId !== userId),
     }));
   };
 
   setLocalStream = stream => {
-    this.setState({ localStream: stream });
+    this.setState({localStream: stream});
   };
 
   resetState = () => {
@@ -116,7 +118,7 @@ export default class VideoScreen extends React.Component {
     });
   };
 
-  _setUpListeners () {
+  _setUpListeners() {
     ConnectyCube.videochat.onCallListener = this._onCallListener;
     ConnectyCube.videochat.onAcceptCallListener = this._onAcceptCallListener;
     ConnectyCube.videochat.onRejectCallListener = this._onRejectCallListener;
@@ -127,7 +129,7 @@ export default class VideoScreen extends React.Component {
 
   _onPressAccept = () => {
     CallService.acceptCall(this._session).then(stream => {
-      const { opponentsIDs, initiatorID, currentUserID } = this._session;
+      const {opponentsIDs, initiatorID, currentUserID} = this._session;
       const opponentsIds = [initiatorID, ...opponentsIDs].filter(
         userId => currentUserID !== userId,
       );
@@ -191,7 +193,7 @@ export default class VideoScreen extends React.Component {
       .catch(this.hideInomingCallModal);
   };
 
-  render () {
+  render() {
     const {
       localStream,
       remoteStreams,
@@ -205,19 +207,21 @@ export default class VideoScreen extends React.Component {
       ? CallService.getUserById(this._session.initiatorID, 'name')
       : '';
     const localStreamItem = localStream
-      ? [{ userId: 'localStream', stream: localStream }]
+      ? [{userId: 'localStream', stream: localStream}]
       : [];
     const streams = [...remoteStreams, ...localStreamItem];
 
     CallService.setSpeakerphoneOn(remoteStreams.length > 0);
 
     return (
-      <SafeAreaView style={{ flex: 1, backgroundColor: 'black' }}>
+      <SafeAreaView style={{flex: 1, backgroundColor: 'black'}}>
         <StatusBar backgroundColor="black" barStyle="light-content" />
         <RTCViewGrid streams={streams} />
         <UsersSelect
           isActiveSelect={isActiveSelect}
           opponentsIds={this.opponentsIds}
+          currentUserLoginId={this.currentUserLoginId}
+          currentUserFullName={this.currentUserFullName}
           selectedUsersIds={selectedUsersIds}
           selectUser={this.selectUser}
           unselectUser={this.unselectUser}
@@ -247,10 +251,10 @@ export default class VideoScreen extends React.Component {
           onCancelPressed={this._onPressReject}
           onConfirmPressed={this._onPressAccept}
           onDismiss={this.hideInomingCallModal}
-          alertContainerStyle={{ zIndex: 1 }}
-          titleStyle={{ fontSize: 21 }}
-          cancelButtonTextStyle={{ fontSize: 18 }}
-          confirmButtonTextStyle={{ fontSize: 18 }}
+          alertContainerStyle={{zIndex: 1}}
+          titleStyle={{fontSize: 21}}
+          cancelButtonTextStyle={{fontSize: 18}}
+          confirmButtonTextStyle={{fontSize: 18}}
         />
       </SafeAreaView>
     );
